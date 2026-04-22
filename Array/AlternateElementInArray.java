0ae160f58e586276a30dd8722259cedaabcdd650
Rutuja8685 <rutujakarande868585@gmail.com>
2026-04-23 01:44:40 +0530

    solved leetcode-75  array easy problem of childrens and their candies

--- Array/AlternateElementInArray.java
+++ Array/AlternateElementInArray.java
@@ -3,11 +3,23 @@ import java.util.*;
 
 class A{
 static void Alternate(int[] arr){
-    for(int i=0; )
+    int n=arr.length;
+    for(int i=0; i<=n; i++){
+    if(i%2==0){
+        System.out.println(arr[i]);
+    }
+}
 }
 }
 public class AlternateElementInArray {
     public static void main(String[] args) {
+        int[] arr={8,7,6,5,4,3,2,77};
+        A a=new A();
 
+        a.Alternate(arr);
+
+        for(int i=0; i<=arr.length; i++){
+            System.out.println(arr[i]);
+        }
     }
 }
